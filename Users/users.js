899f0b6cd88055e086d397c9d4bfdcacdf4296c8
Adilet Zhusupov <adilet.zhusupov@gmail.com>
2022-09-26 06:47:41 +0000

    Moved Users/users.js

---
+++ Users/users.js
@@ -0,0 +1,37 @@
+const users = [
+  {
+    fname: "Jacynthe",
+    lname: "Hand",
+    password: "password 1",
+    email: "Monte56@hotmail.com",
+    id: "1"
+  },
+  {
+    fname: "Telly",
+    lname: "Russel",
+    password: "password 2",
+    email: "Berenice_West93@gmail.com",
+    id: "2"
+  },
+  {
+    fname: "Eldridge",
+    lname: "Schiller",
+    password: "password 3",
+    email: "Peyton.Feeney@gmail.com",
+    id: "3"
+  },
+  {
+    fname: "Hortense",
+    lname: "Carter",
+    password: "password 4",
+    email: "Sylvester.Hammes@yahoo.com",
+    id: "4"
+  },
+  {
+    fname: "Gustave",
+    lname: "Adams",
+    password: "password 5",
+    email: "Nigel60@yahoo.com",
+    id: "5"
+  }
+];
